d7a54276f4fd888f4c89a2391828ebd9becd7886
CDotNightHawk <nighthawk@nanofox.dev>
2026-04-29 04:02:21 -0400

    Fixed adding bitwarden

--- lib/firefox-policies.nix
+++ lib/firefox-policies.nix
@@ -77,7 +77,7 @@ in
       install_url = "https://addons.mozilla.org/firefox/downloads/latest/nekocap/latest.xpi";
       installation_mode = "force_installed";
     };
-    "hello@bitwarden.com" = {
+    "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
       install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
       installation_mode = "force_installed";
     };
